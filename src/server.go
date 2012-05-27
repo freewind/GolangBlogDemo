fcb4f1a2725330f8e687af5844c2eab7b56acfec
Freewind <nowind_lee@qq.com>
2012-05-27 23:46:47 +0800

    mustache example and html/template example

--- src/server.go
+++ src/server.go
@@ -3,23 +3,31 @@ package main
 import (
 	"code.google.com/p/gorilla/mux"
 	"fmt"
-	"github.com/hoisie/mustache"
+	mus "github.com/hoisie/mustache"
+	"html/template"
 	"net/http"
 )
 
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", index)
-	r.HandleFunc("/must", must)
+	r.HandleFunc("/mustache", mustache)
+	r.HandleFunc("/tmpl", tmpl)
 	http.Handle("/", r)
-	http.ListenAndServe("0.0.0.0:3000", nil)
+	http.ListenAndServe(":3000", nil)
 }
 
 func index(res http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(res, "hello, golang!")
 }
 
-func must(res http.ResponseWriter, req *http.Request) {
-	data := mustache.Render("Hello, {{name}}!", map[string]string{"name": "mustache"})
+func tmpl(res http.ResponseWriter, req *http.Request) {
+	t := template.New("test")
+	t, _ = t.Parse("hello, {{.Name}}!")
+	t.Execute(res, map[string]string{"Name": "html/template"})
+}
+
+func mustache(res http.ResponseWriter, req *http.Request) {
+	data := mus.Render("Hello, {{name}}!", map[string]string{"name": "mustache"})
 	fmt.Fprintf(res, data)
 }
