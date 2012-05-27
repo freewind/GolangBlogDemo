bb45546fea5ef959f54407f987de4e1267e9d442
Freewind <nowind_lee@qq.com>
2012-05-27 16:21:05 +0800

    add mustache example

--- src/server.go
+++ src/server.go
@@ -3,16 +3,23 @@ package main
 import (
 	"code.google.com/p/gorilla/mux"
 	"fmt"
+	"github.com/hoisie/mustache"
 	"net/http"
 )
 
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", index)
+	r.HandleFunc("/must", must)
 	http.Handle("/", r)
 	http.ListenAndServe("0.0.0.0:3000", nil)
 }
 
 func index(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(res, "Hello, golang!")
+	fmt.Fprintf(res, "hello, golang!")
+}
+
+func must(res http.ResponseWriter, req *http.Request) {
+	data := mustache.Render("Hello, {{name}}!", map[string]string{"name": "mustache"})
+	fmt.Fprintf(res, data)
 }
